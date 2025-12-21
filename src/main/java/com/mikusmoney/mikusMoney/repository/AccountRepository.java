708f93b587a9e892e6fe69a7bdef53f0bfbc1fc8
juan17la <juandiegolopezarias07@gmail.com>
2025-12-21 11:45:10 -0500

    creating account repository

---
+++ src/main/java/com/mikusmoney/mikusMoney/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.mikusmoney.mikusMoney.repository;
+
+import com.mikusmoney.mikusMoney.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    
+    Optional<Account> findByMikuId(Long mikuId);
+}
