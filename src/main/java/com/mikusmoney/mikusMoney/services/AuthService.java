da5475138dbc87fea3e1dbefec0f5de5f1cc524d
juan17la <juandiegolopezarias07@gmail.com>
2025-12-27 21:05:03 -0500

    Creating services for authentications process. Register, Login, Logout and me functions sucessfully return token generation and validations

--- src/main/java/com/mikusmoney/mikusMoney/services/AuthService.java
+++ src/main/java/com/mikusmoney/mikusMoney/services/AuthService.java
@@ -13,7 +13,6 @@ import com.mikusmoney.mikusMoney.repository.MikuRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
