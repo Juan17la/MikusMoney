b131841d32fa89e3bc5c91e392dca8b57cf6940d
juan17la <juandiegolopezarias07@gmail.com>
2025-12-29 17:01:35 -0500

    Refactor to Account repository to use @Lock for @version atribute

--- src/main/java/com/mikusmoney/mikusMoney/repository/AccountRepository.java
+++ src/main/java/com/mikusmoney/mikusMoney/repository/AccountRepository.java
@@ -1,7 +1,12 @@
 package com.mikusmoney.mikusMoney.repository;
 
 import com.mikusmoney.mikusMoney.entity.Account;
+
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.Version;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +14,6 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Account> findByMikuId(Long mikuId);
 }
