df053efe6cc0e004965bb7d7d510d4c82a126dc5
juan17la <juandiegolopezarias07@gmail.com>
2026-01-10 12:06:28 -0500

    Add findPigsByMikuId query to SavingsPigRepository for retrieving user's savings pigs

--- src/main/java/com/mikusmoney/mikusMoney/repository/SavingsPigRepository.java
+++ src/main/java/com/mikusmoney/mikusMoney/repository/SavingsPigRepository.java
@@ -1,5 +1,7 @@
 package com.mikusmoney.mikusMoney.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +11,7 @@ import com.mikusmoney.mikusMoney.entity.SavingsPig;
 public interface SavingsPigRepository extends JpaRepository<SavingsPig, Long> {
     @Query("SELECT COUNT(sp) FROM SavingsPig sp WHERE sp.miku.id = :mikuId AND sp.broken = false")
     int getNumberOfPigsByOwnerId(@Param("mikuId") Long mikuId);
+
+    @Query("SELECT sp FROM SavingsPig sp WHERE sp.miku.id = :mikuId AND sp.broken = false")
+    List<SavingsPig> findPigsByMikuId(@Param("mikuId") Long mikuId);
 }
