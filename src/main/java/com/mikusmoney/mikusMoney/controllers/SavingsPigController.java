c07be9a8f2df1aebf12fb4fafd94c4230592dd7b
juan17la <juandiegolopezarias07@gmail.com>
2026-01-14 10:32:09 -0500

    Add breakSavingsPig method and endpoint to handle savings pig break requests

--- src/main/java/com/mikusmoney/mikusMoney/controllers/SavingsPigController.java
+++ src/main/java/com/mikusmoney/mikusMoney/controllers/SavingsPigController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigBreakRequest;
 import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigCreationRequest;
 import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigDepositRequest;
 import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigResponse;
@@ -18,18 +19,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/v0.1/savings/pig/")
 @RequiredArgsConstructor
 public class SavingsPigController {
-    
+
     private final SavingsPigService savingsPigService;
 
     @PostMapping("create")
     public ResponseEntity<SavingsPigResponse> createSavingsPig(@Valid @RequestBody SavingsPigCreationRequest request) {
-        
         return ResponseEntity.ok(savingsPigService.createSavingsPig(request));
     }
 
@@ -37,7 +36,12 @@ public class SavingsPigController {
     public ResponseEntity<SavingsPigResponse> saveMoney(@Valid @RequestBody SavingsPigDepositRequest request) {
         return ResponseEntity.ok(savingsPigService.depositMoney(request));
     }
-    
+
+    @PostMapping("break")
+    public ResponseEntity<SavingsPigResponse> breakSavingsPig(@Valid @RequestBody SavingsPigBreakRequest request) {
+        return ResponseEntity.ok(savingsPigService.brakeSavingsPig(request));
+    }
+
     @GetMapping("all")
     public ResponseEntity<List<SavingsPigResponse>> getAllSavingsPigs() {
         return ResponseEntity.ok(savingsPigService.getSavingsPigs());
--- src/main/java/com/mikusmoney/mikusMoney/services/SavingsPigService.java
+++ src/main/java/com/mikusmoney/mikusMoney/services/SavingsPigService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigBreakRequest;
 import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigCreationRequest;
 import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigDepositRequest;
 import com.mikusmoney.mikusMoney.dto.savingsDTOs.SavingsPigResponse;
+import com.mikusmoney.mikusMoney.services.operations.BreakSavingsOperation;
 import com.mikusmoney.mikusMoney.services.operations.CreateSavingsPigOperation;
 import com.mikusmoney.mikusMoney.services.operations.GetSavingsPigsOperation;
 import com.mikusmoney.mikusMoney.services.operations.SavingsSaveOperation;
@@ -16,21 +18,25 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class SavingsPigService {
-    
 
-    private final CreateSavingsPigOperation createSavingsPig;
+    private final CreateSavingsPigOperation createSavingsPigOperation;
     private final GetSavingsPigsOperation getSavingsPigsOperation;
     private final SavingsSaveOperation savingsDepositOperation;
-    
+    private final BreakSavingsOperation breakSavingsOperation;
+
     public SavingsPigResponse createSavingsPig(SavingsPigCreationRequest request) {
-        return createSavingsPig.execute(request);
+        return createSavingsPigOperation.execute(request);
     }
 
-    public List<SavingsPigResponse> getSavingsPigs() {
-        return getSavingsPigsOperation.execute(null);
+    public SavingsPigResponse brakeSavingsPig(SavingsPigBreakRequest request) {
+        return breakSavingsOperation.execute(request);
     }
 
     public SavingsPigResponse depositMoney(SavingsPigDepositRequest request) {
         return savingsDepositOperation.execute(request);
+    }
+
+    public List<SavingsPigResponse> getSavingsPigs() {
+        return getSavingsPigsOperation.execute(null);
     }
 }
